ad6f1e856e2fd2a2146f6d52064bbe55a21f9f43
Janaina Gomes da Silva <21944@student.dorset-college.ie>
2022-02-10 15:26:17 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Exercise11
+{
+    class Program
+    {
+        /*Write a program for a package delivery service. The program contains an array that holds the 
+            ten zip codes to which the company delivers packages. Prompt a user to enter a zip code and 
+            display a message indicating whether the zip code is one to which the company delivers.*/
+        static void Main(string[] args)
+        {
+
+            string[] zipCode = {"D01A3M2","D10I9R0", "D02F6H7", "D14S2S2","D01H2N2"  };
+
+            Console.WriteLine("Please insert your zip code: ");
+            string zipCodeUser = Console.ReadLine();
+
+            bool contains = false;
+
+
+            for (int i = 0; i < zipCode.Length; i++)
+            {
+                if (zipCodeUser == zipCode[i])
+                {
+                    contains = true;
+                    break;
+                }
+            }
+
+            if (contains == true)
+            {
+                Console.WriteLine("We deliver to you home");
+                
+            }
+            else
+            {
+                Console.WriteLine("We don't deliver to you home");
+            }
+
+
+        }
+    }
+}
